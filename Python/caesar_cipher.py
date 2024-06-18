bc69f598fc8d62fb00c289babda1bc6a8ae0cf0d
Myster <97202292+MysterV@users.noreply.github.com>
2024-06-18 20:08:54 +0200

    Update caesar_cipher.py
    - removed the assets.py dependence for alphabet
    - replaced the underscore with an i
    - removed unnecessary code
    - added direction aliases

--- Python/caesar_cipher.py
+++ Python/caesar_cipher.py
@@ -1,35 +1,32 @@
 def caesar(direction, message, shift='1'):
-    if direction != 'encrypt' and direction != 'decrypt':
-        raise Exception('Invalid syntax! Expected "encrypt" or "decrypt" for direction.')
-    if not shift.isnumeric(): raise Exception('Invalid syntax! Expected an integer.')
-
-    shift = int(shift)
-    if direction == 'decrypt': shift *= -1
-    from assets import alphabet # contains both lowercase and uppercase letters
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     message_processed = ''
+    shift = int(shift)
+    
+    if direction not in ('encrypt', 'e', 'en', 'decrypt', 'd', 'de'):
+        raise Exception('Invalid syntax! Expected "encrypt" or "decrypt" for direction.')
+    if direction in ('decrypt', 'd', 'de'): shift *= -1
 
-    for _ in range(len(message)):
-        if message[_] not in alphabet: # account for non-letter characters
-            message_processed += message[_]
-
+    for i in range(len(message)):
+        if message[i] not in alphabet: message_processed += message[i]
+        
         else:
-            letter_index = (alphabet.index(message[_])+shift)%26
-            if message[_].isupper(): letter_index += 26 # account for uppercase letters
-            
+            letter_index = (alphabet.index(message[i]) + shift) % 26
+            if message[i].isupper(): letter_index += 26
             message_processed += alphabet[letter_index]
 
+    return message_processed
 
-    print(message_processed)
 
 def run():
-    try:
-        caesar(
-            input('Do you wish to "encrypt" or "decrypt"?\n'),
-            input('What is the message we\'re working with?\n'),
-            input('What is the shift we\'re working with?\n'))
-    except:
-        print('Wrong syntax!')
+    print(caesar(
+        input('Do you wish to "encrypt" or "decrypt"?\n'),
+        input('What is the message we\'re working with?\n'),
+        input('What is the shift we\'re working with?\n')))
+
+    if input('Run the cipher program again? y/n\n') == 'y':
+        print('')
         run()
-    if input('Do you want to run the cipher program again? y/n\n') == 'y': run()
 
 run()
