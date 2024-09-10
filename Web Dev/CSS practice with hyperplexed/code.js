c208d5b4b7dc21c80738d0bacc8178ad03c20f26
Myster <97202292+MysterV@users.noreply.github.com>
2024-09-10 18:31:51 +0200

    Removed a JS comment
    Who knew fixing the code was this simple!

--- Web Dev/CSS practice with hyperplexed/code.js
+++ Web Dev/CSS practice with hyperplexed/code.js
@@ -20,7 +20,7 @@ window.onmousemove = e => {
     nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
 
   track.dataset.percentage = nextPercentage;
-/*
+
   track.animate({
     transform: `translate(${nextPercentage}%, -50%)`
   }, {duration:2400, fill:"forwards"}); 
@@ -29,7 +29,7 @@ window.onmousemove = e => {
     image.animate({
       objectPosition: `${100 + nextPercentage}% center`
     }, { duration: 1200, fill: "forwards" });
-  }*/
+  }
 }
 
 
@@ -65,8 +65,8 @@ const handleOnMove = e => {
   }
 }
 
-/* -- Had to add extra lines for touch events -- */
-/*
+// ==========
+
 window.onmousedown = e => handleOnDown(e);
 
 window.ontouchstart = e => handleOnDown(e.touches[0]);
