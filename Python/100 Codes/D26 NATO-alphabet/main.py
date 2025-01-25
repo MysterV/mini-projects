24eb569ca47178ec2730d2e1fae1c48410807a39
Myster <97202292+MysterV@users.noreply.github.com>
2025-01-26 00:27:38 +0100

    Made the NATO alphabet more useable through a file

--- Python/100 Codes/D26 NATO-alphabet/main.py
+++ Python/100 Codes/D26 NATO-alphabet/main.py
@@ -1,8 +1,12 @@
 import pandas
+import os
 
 nato_alphabet = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_alphabet_dict = {row.letter: row.code for (index, row) in nato_alphabet.iterrows()}
 
 phrase = input('Phrase: ').upper()
 phrase_code = [nato_alphabet_dict[letter] for letter in phrase.upper() if letter.isalpha()]
-print(phrase_code)
+
+with open('output.txt', 'wt') as f:
+    f.write(' '.join(phrase_code))
+os.startfile('output.txt')
