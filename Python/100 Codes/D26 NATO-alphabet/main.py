e82d37dfe1c114a00b31678e3e0eab67f4c1b739
Myster <97202292+MysterV@users.noreply.github.com>
2025-01-26 00:52:36 +0100

    Over-engineered NATO alphabet can now convert ANY unicode character to a word representation of it

--- Python/100 Codes/D26 NATO-alphabet/main.py
+++ Python/100 Codes/D26 NATO-alphabet/main.py
@@ -1,11 +1,20 @@
 import pandas
 import os
+import inflect
+import unicodedata
 
 nato_alphabet = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_alphabet_dict = {row.letter: row.code for (index, row) in nato_alphabet.iterrows()}
 
 phrase = input('Phrase: ').upper()
-phrase_code = [nato_alphabet_dict[letter] for letter in phrase.upper() if letter.isalpha()]
+phrase_code = []
+for letter in phrase.upper():
+    if letter.isalpha():
+        phrase_code.append(nato_alphabet_dict[letter])
+    elif letter.isnumeric():
+        phrase_code.append(inflect.engine().number_to_words(letter).title())
+    else:
+        phrase_code.append(unicodedata.name(letter).title())
 
 with open('output.txt', 'wt') as f:
     f.write(' '.join(phrase_code))
