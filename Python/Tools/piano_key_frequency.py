936d468b03e43adcf9c0c12ab5dbea8c81f4693d
Myster <97202292+MysterV@users.noreply.github.com>
2025-04-12 20:05:29 +0200

    Update piano_key_frequency.py
    Alr, finished. Wow, this turned out more coherent than I expected!
    On a side note, solving LeetCode problems seems to have taught me how to properly introduce the functions

--- Python/Tools/piano_key_frequency.py
+++ Python/Tools/piano_key_frequency.py
@@ -1,27 +1,42 @@
 import re
 
-def piano_key_wave(key: str, tuning: float=440.0, tuning_key: str='A4') -> dict:
-    '''
-    Takes a key in the form \<note name>\<accidental>\<octave>, e.g. A#4,
-    and returns the frequency of that note.
-    \nSupports multi-accidentals and notes outside of the a piano's scale
-    '''
+def get_note_midi_number(key: str) -> int:
+    '''returns the MIDI number of the note'''
     key_analyzed = re.match('(^[a-g]{1})([b#]*)(-*\d+)', key.lower())
+    if not key_analyzed: raise Exception('Incorrect note format')
+
     note, accidental, octave = key_analyzed.groups()
     octave = int(octave)
-    notes = ['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#']
-
-    note_number = notes.index(note)  # scale degree, 1-7
+    notes = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b']
+    note_number = (notes.index(note) + (octave+1)*12)
+    
     if accidental:
-        for b in range(accidental.count('b')):
-            note_number -= 1  # down a semitone
-        for sharp in range(accidental.count('#')):
-            note_number += 1  # up a semitone
+        for b in range(accidental.count('b')): note_number -= 1  # down a semitone
+        for sharp in range(accidental.count('#')): note_number += 1  # up a semitone
+    return note_number
 
-    return tuning * 2 ** (octave-4 + (note_number)/12)
-    # moves you down to A0, and then moves you up to the right octave, then shifts to the right degree
+def piano_key_wave(key: int|str='A4', tuning: float=440.0, tuning_ref_key: int|str='A4', round_digits: int=3) -> dict:
+    '''
+    Takes in the key in the form of either:
+    \n- {1 letter note name}{however many accidentals you want}{octave number}, e.g. A#4, D##b#b11,
+    \n- the MIDI number of the note, e.g. 70,
+    \nReturns the frequency of that note.
+    \nSupports multi-accidentals and notes outside of the a piano's scale
+    '''
+    # if the input is the key name, e.g. A#4, get the MIDI number of the keys first
+    if isinstance(key, str):
+        note_number = get_note_midi_number(key)
+    elif isinstance(key, int): note_number = key
 
-notes = ['a4', 'a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5', 'e5', 'f5', 'f#5', 'g5', 'g#5']
-for i in notes:
-    print(piano_key_wave(i))
+    if isinstance(tuning_ref_key, str):
+        ref_note_number = get_note_midi_number(tuning_ref_key)
+    elif isinstance(tuning_ref_key, int): ref_note_number = tuning_ref_key
+    
+    return round((tuning * 2 ** ((note_number-ref_note_number)/12)), round_digits)
+
+
+test = ['c0', 'a4', 'a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5', 'e5', 'f5', 'f#5', 'g5', 'g#5', 'A5', 'a##5', 'dbbbbb5', 'Ab#b#b#b#b#5', 'A-1', 'C-10', 'C12', 'D20', 69, 1024]
+print(piano_key_wave('A4'))
+for i in test:
+    print(piano_key_wave(i, tuning=261.626, tuning_ref_key=60))
 
