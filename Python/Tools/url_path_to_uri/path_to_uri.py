6ab22d6a036f2b5b563ec58fe65808563478bb0a
Myster <97202292+MysterV@users.noreply.github.com>
2025-04-26 17:36:48 +0200

    Separate function from caller

--- Python/Tools/url_path_to_uri/path_to_uri.py
+++ Python/Tools/url_path_to_uri/path_to_uri.py
@@ -30,16 +30,3 @@ def to_uri(path: str, protocol: str='file://', address: str='/'):
         return protocol + address + drive_letter + '/' + path
     else:
         return protocol + address + path
-
-# use example
-addresses = {
-    'file://': '/',
-    'http://': 'localhost:8080/'
-}
-
-while True:
-    path = input('Path to encode (leave empty to exit):\n')
-    if not path:
-        break
-    for protocol, address in addresses.items():
-        print(to_uri(path=path, protocol=protocol, address=address))
